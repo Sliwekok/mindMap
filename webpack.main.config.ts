046c9c37b128369df7d7ed24f7bab3d81576c708
sliwekok <dominiksliwek@gmail.com>
2024-11-24 21:32:43 +0100

    added icon

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -24,6 +24,6 @@ export const mainConfig: Configuration = {
   },
   plugins,
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json', '.ico'],
   },
 };
